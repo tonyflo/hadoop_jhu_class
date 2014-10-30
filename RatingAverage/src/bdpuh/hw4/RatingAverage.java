ccf389c7a173e680e21b5db1f3ff5e372e8b9b20
Tony Florida <tflorid1@jhu.edu>
2014-10-29 21:55:05 -0400

    Copy and rename of HW3

---
+++ RatingAverage/src/bdpuh/hw4/RatingAverage.java
@@ -0,0 +1,69 @@
+/*
+ * Tony Florida
+ * 2014-10-29
+ * JHU - Big Data Processing with Hadoop
+ * Assignment 4
+ */
+package bdpuh.hw4;
+
+import java.io.IOException;
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.Text;
+
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.input.TextInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.mapreduce.lib.output.TextOutputFormat;
+
+public class RatingAverage {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Job wordCountJob = null;
+        
+        Configuration conf = new Configuration();
+        try{
+            wordCountJob = Job.getInstance(conf, "RatingDistribution");
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        
+        //Specify the input path
+        try{
+            FileInputFormat.addInputPath(wordCountJob, new Path(args[0]));
+        } catch (IOException ex) {
+            System.out.println(ex);
+        }
+        
+        // set the input data format
+        wordCountJob.setInputFormatClass(TextInputFormat.class);
+        
+        // set the mapper and reducer class
+        wordCountJob.setMapperClass(RatingAverageMapper.class);
+        wordCountJob.setReducerClass(RatingAverageReducer.class);
+        
+        // set the jar file
+        wordCountJob.setJarByClass(bdpuh.hw4.RatingAverage.class);
+        
+        // set the output path
+        FileOutputFormat.setOutputPath(wordCountJob, new Path(args[1]));
+        
+        // set the output data format
+        wordCountJob.setOutputFormatClass(TextOutputFormat.class);
+        
+        // set the output key and value class
+        wordCountJob.setOutputKeyClass(Text.class);
+        wordCountJob.setOutputValueClass(IntWritable.class);
+        
+        try {
+            wordCountJob.waitForCompletion(true);
+        } catch (IOException | InterruptedException | ClassNotFoundException ex) {
+            System.out.println(ex);
+        }
+    }
+}
---
+++ RatingAverage/src/bdpuh/hw4/RatingAverageMapper.java
@@ -0,0 +1,33 @@
+package bdpuh.hw4;
+
+import java.io.IOException;
+import org.apache.hadoop.io.IntWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class RatingAverageMapper extends Mapper<LongWritable, Text, Text, IntWritable>{
+    IntWritable one = new IntWritable(1);
+    Text rating = new Text();
+    
+    @Override
+    protected void setup(Context context) throws IOException, InterruptedException {
+        super.setup(context);
+        System.out.println("in setup of " + context.getTaskAttemptID().toString());
+        String filename = ((FileSplit) context.getInputSplit()).getPath() + "";
+        System.out.println("in stdout" + context.getTaskAttemptID().toString() + " " + filename);
+        System.err.println("in stderr" + context.getTaskAttemptID().toString());
+    }
+    
+    @Override
+    protected void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+        String line = value.toString();
+        //StringTokenizer tokenizer = new StringTokenizer(line);
+        String lineArray[] = line.split("\t");
+        //while(tokenizer.hasMoreTokens()) {
+            rating.set(lineArray[2]);
+            context.write(rating, one);
+        //}
+    }
+}
